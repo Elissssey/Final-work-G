75f95a36aa3e8e2a6e7e15afe923ac93dec3897c
Elissssey <eliseev.rds@gmail.com>
2023-03-25 12:12:57 +0300

    Продолжаем работу над программой и создаем еще один массив со строками

--- Program.cs
+++ Program.cs
@@ -8,3 +8,16 @@ class Program
         Console.Write("Введите строку элементов через запятую: ");
         string input = Console.ReadLine();
         string[] strings = input.Split(new char[] {','});
+
+        // Создание нового массива с подходящими строками
+        string[] output = new string[strings.Length];
+        int count = 0;
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (strings[i].Length <= 3)
+            {
+                output[count] = strings[i];
+                count++;
+            }
+        }
+        
