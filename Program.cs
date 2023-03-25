e606766f12338fc7ebc588de2f441b04fd988179
Elissssey <eliseev.rds@gmail.com>
2023-03-25 12:12:00 +0300

    Начали работу над программой и обозначили исходный массив данных

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        // Ввод исходного массива строк с клавиатуры
+        Console.Write("Введите строку элементов через запятую: ");
+        string input = Console.ReadLine();
+        string[] strings = input.Split(new char[] {','});
